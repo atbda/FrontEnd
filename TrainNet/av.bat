2c69c5e45b3aa349e5441c5677a2c675f2c00e3d
atbda <36071494+atbda@users.noreply.github.com>
2018-04-02 15:40:00 +0530

    Add files via upload

---
+++ TrainNet/av.bat
@@ -0,0 +1,4 @@
+start AVPlayer.exe -t -x 650 -y 420 -p 1101 -ap 2201 -ip 225.1.2.200
+start AVPlayer.exe -t -x 650 -y 100 -p 1102 -ap 2202 -ip 225.1.2.200
+start AVPlayer.exe -t -x 294 -y 100 -p 1103 -ap 2203 -ip 225.1.2.200
+start AVPlayer.exe -t -x 294 -y 420 -p 1104 -ap 2204 -ip 225.1.2.200
